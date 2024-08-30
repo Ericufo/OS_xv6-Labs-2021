af97a008eef613b3d8586152a9e70a8ccc047a3e
eric <ericouufo@gmail.com>
2024-08-30 11:33:59 +0800

    lab 10: mmap

--- kernel/memlayout.h
+++ kernel/memlayout.h
@@ -50,6 +50,8 @@
 // map the trampoline page to the highest address,
 // in both user and kernel space.
 #define TRAMPOLINE (MAXVA - PGSIZE)
+// the minimum address the mmap can use - lab10
+#define MMAPMINADDR (TRAPFRAME - 10 * PGSIZE)
 
 // map kernel stacks beneath the trampoline,
 // each surrounded by invalid guard pages.
